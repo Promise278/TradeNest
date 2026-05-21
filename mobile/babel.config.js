ba5a8d577280cc80f55085c828eebfe227d2f32e
Promise278 <promiseobi2007@gmail.com>
2026-05-21 10:25:29 +0100

    fixs: implemented backend folder added packages needed and also made the mobile folder visible in github

---
+++ mobile/babel.config.js
@@ -0,0 +1,8 @@
+// module.exports = {
+//   presets: ['module:@react-native/babel-preset'],
+//   plugins: ["nativewind/babel", "react-native-worklets/plugin"],
+// };
+module.exports = {
+  presets: ["module:@react-native/babel-preset"],
+  plugins: ["react-native-worklets/plugin"],
+};
---
+++ mobile/metro.config.js
@@ -0,0 +1,18 @@
+// const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+// const { withNativeWind } = require("nativewind/metro");
+
+// /**
+//  * Metro configuration
+//  * https://reactnative.dev/docs/metro
+//  *
+//  * @type {import('@react-native/metro-config').MetroConfig}
+//  */
+// const config = getDefaultConfig(__dirname);
+
+// module.exports = withNativeWind(mergeConfig(config, {}), { input: "./global.css" });
+const { getDefaultConfig } = require("@react-native/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config);
