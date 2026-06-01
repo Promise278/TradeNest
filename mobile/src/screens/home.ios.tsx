201256f64b937d687e1c12f2fdcfb86ac372cbac
Promise278 <promiseobi2007@gmail.com>
2026-06-01 12:50:07 +0100

    feat: implemmented onboarding screen features

---
+++ mobile/src/screens/home.ios.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View>
+      <Text>THESE IS THE IOS APP</Text>
+    </View>
+  )
+}
